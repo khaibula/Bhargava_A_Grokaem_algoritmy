bb7fee26e94c313c42befe4fd692b35cfb6fb6a4
khaibula <gaziev.777@bk.ru>
2023-05-28 17:35:36 +0300

    add search in graph

---
+++ 6.1.search-in-graph.js
@@ -0,0 +1,33 @@
+const findOdnolybInGraph = (graph) => {
+  const queue = [...graph.you];
+
+  for (let i = 0; i <= queue.length; i++) {
+    const friendName = queue[i];
+
+    if (graph[friendName] && graph[friendName].length === 1) {
+      console.log('i', i)
+      return friendName;
+    }
+
+    if (graph[friendName]) {
+      const friends = graph[friendName]
+        .filter((friend) => !queue.includes(friend));
+
+      queue.push(...friends);
+    }
+  }
+}
+
+const obj = {
+  you: ['vasuy', 'sasha', 'vova'],
+  vasuy: ['alina', 'katya', 'natasha'],
+  sasha: ['kirill', 'anna', 'liza', 'kolya'],
+  kolya: ['kirill', 'anna', 'liza'],
+  vova: ['irina', "vika", "lera"],
+  irina: [ 'andrey', 'alena'],
+  vika: ['dima', 'victor', 'anar'],
+  anar: ['olya'],
+}
+
+console.log(findOdnolybInGraph(obj))
+
